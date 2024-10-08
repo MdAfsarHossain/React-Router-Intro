9e5d2ff97520938167db25323e397c5d50a92ed6
MdAfsarHossain <afsar291722@gmail.com>
2024-10-08 09:34:15 +0600

    About jsx file added

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div className="flex flex-col gap-2 py-5">
+            <h1 className="text-2xl">This is About Section</h1>            
+            <p>Welcome to the about section</p>
+        </div>
+    );
+};
+
+export default About;
