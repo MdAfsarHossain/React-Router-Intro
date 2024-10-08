110f77f48c4a1465e80d7aa4c4f834cd4c762093
MdAfsarHossain <afsar291722@gmail.com>
2024-10-08 09:34:32 +0600

    Contact jsx file added

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+
+const Contact = () => {
+    return (
+        <div className="flex flex-col gap-2 py-5">
+            <h1 className="text-2xl">This is Contact Section</h1>            
+            <p>Welcome to the Contact Section</p>
+        </div>
+    );
+};
+
+export default Contact;
