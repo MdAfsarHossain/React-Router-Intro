1d7d35bb0fdc9e847371cd447827c661d42293c4
MdAfsarHossain <afsar291722@gmail.com>
2024-10-08 09:35:11 +0600

    Footer jsx file added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-white">Thank you for visiting my website.</h1>            
+        </div>
+    );
+};
+
+export default Footer;
